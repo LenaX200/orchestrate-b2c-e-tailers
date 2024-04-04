8b1f85c664768a6af724ff01b960fa94c4ba770b
ElenaIT <156838043+LenaX200@users.noreply.github.com>
2024-04-04 04:37:50 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,16 @@
+}
+    const data = generateRandomData();
+    }
+    const data = [];
+    return data;
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+main();
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+
+
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    });
